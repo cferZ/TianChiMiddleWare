2422216ef23035a5606e9e5cfe5b345cc434cbaa
CferZ <472770699@qq.com>
2017-05-18 17:20:10 +0800

    17：19 2017-5-18

--- src/io/openmessaging/demo/MessageStore.java
+++ src/io/openmessaging/demo/MessageStore.java
@@ -2,6 +2,8 @@ package io.openmessaging.demo;
 
 import io.openmessaging.Message;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -11,13 +13,27 @@ import java.util.Set;
 
 public class MessageStore {
 
+	private final int LOOPINTERVAL=1000;
+	private final int MAXMESSAGELENGTH=256*1024;
+	private final String OFFSETFILE="";
     private static final MessageStore INSTANCE = new MessageStore();
-    
-    private static final HashMap<String,Integer[]> fileIndex=new HashMap<>();
-    
+     
     private static final HashMap<String,FileChannel> fileHandlers=new HashMap<>();
     
+    private static final HashMap<String , Integer> bucketIndex=new HashMap<>();
     
+    private Thread loopSaveHandler=null;
+    private Thread indexSaveHandler=null;							
+    public MessageStore() {
+		// TODO Auto-generated constructor stub
+    	//TODO 从文件中读queueOffset
+    	
+    	
+    	loopSaveHandler=new Thread(new LoopSave());
+    	loopSaveHandler.start();
+    	indexSaveHandler=new Thread(new IndexSave());
+    	indexSaveHandler.start();
+	}
 
     public static MessageStore getInstance() {
         return INSTANCE;
@@ -35,7 +51,7 @@ public class MessageStore {
         bucketList.add(message);
     }
 
-   public synchronized Message pullMessage(String queue, String bucket) {
+    public synchronized Message pullMessage(String queue, String bucket) {
         ArrayList<Message> bucketList = messageBuckets.get(bucket);
         if (bucketList == null) {
             return null;
@@ -52,32 +68,63 @@ public class MessageStore {
         Message message = bucketList.get(offset);
         offsetMap.put(bucket, ++offset);
         return message;
-   }
+    }
    
    
-   private class LoopSave implements Runnable{
-	   @Override
-	   public void run() {
+    private class IndexSave implements Runnable{
+	    @Override
+		public void run() {
 			// TODO Auto-generated method stub
-			  Set<String> keys= messageBuckets.keySet();
-			  for(String key:keys){
-				  ArrayList<Message> bucketList=messageBuckets.get(key);
-				  if (bucketList == null) {
-			            continue;
-			        }
-			        HashMap<String, Integer> offsetMap = queueOffsets.get(queue);
-			        if (offsetMap == null) {
-			            offsetMap = new HashMap<>();
-			            queueOffsets.put(queue, offsetMap);
-			        }
-			        int offset = offsetMap.getOrDefault(bucket, 0);
-			        if (offset >= bucketList.size()) {
-			            return null;
-			        }
-			        Message message = bucketList.get(offset);
-			        offsetMap.put(bucket, ++offset);
-			        return message;
-			  }
+	    	ByteBuffer buf=ByteBuffer.allocate(MAXMESSAGELENGTH);
+			try {
+				Thread.sleep(LOOPINTERVAL);
+				FileChannel f=fileHandlers.get(OFFSETFILE);
+				if(f==null){
+					//TODO
+				}
+				else{
+					buf.clear();
+					buf.put(util.serializationUtil.getByteBuffer((HashMap)queueOffsets));
+					f.write(buf);
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+    }
+   
+    private class LoopSave implements Runnable{
+    	@Override
+    	public void run() {
+    		// TODO Auto-generated method stub
+			Set<String> keys= messageBuckets.keySet();
+			for(String key:keys){
+				ArrayList<Message> bucketList=messageBuckets.get(key);
+				if (bucketList == null) {
+			          continue;
+			    }  
+			    int offset = bucketIndex.getOrDefault(key, 0);
+			    if (offset >= bucketList.size()) {
+			        continue;
+			    }
+			    Message message = bucketList.get(offset);
+			    bucketIndex.put(key, ++offset);
+			    //写文件
+			    FileChannel file= fileHandlers.get(key);
+			    if(file==null){
+			    	//TODO
+			    }
+			    try {
+			    	synchronized(file){
+			    		file.write(((DefaultBytesMessage)message).getByteBuffer());
+			    	}
+			    } catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+			    }
+			      
+			}
 	   }
-   }
+    }
 }
